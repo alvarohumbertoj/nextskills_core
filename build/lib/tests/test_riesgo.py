0527291afdf782ec86351ac2ba4c8889dbb2a262
alvarohumbertoj <alvarohumbertojf@gmail.com>
2025-07-25 18:35:36 +0200

    Delete build/lib directory

--- build/lib/tests/test_riesgo.py
+++
@@ -1,6 +0,0 @@
-from nextskills_core.riesgo import calcular_riesgo
-
-def test_calcular_riesgo():
-    assert calcular_riesgo(0.3) == "Bajo"
-    assert calcular_riesgo(0.5) == "Medio"
-    assert calcular_riesgo(0.8) == "Alto"
